e35e8ccc14b1c7db684b24c970a3e040ff0a87da
amir-lahav <Amir.Lahav@shutterfly.com>
2021-04-14 17:25:31 +0300

    main
    Fix cluster algo

--- Sources/LADSA/Algorithm/Clusters/ChineseWhisper.swift
+++ Sources/LADSA/Algorithm/Clusters/ChineseWhisper.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-public struct SamplePair {
+struct SamplePair<T: Comparable> {
     
-    let index1: Int
-    let index2: Int
+    let index1: T
+    let index2: T
     let distance: Double
     
-    init(idx1: Int, idx2: Int, distance: Double = 1) {
+    init(idx1: T, idx2: T, distance: Double = 1) {
         self.distance = distance
         if (idx1 < idx2) {
             index1 = idx1
@@ -25,20 +25,20 @@ public struct SamplePair {
     }
 }
 
-public struct OrderedSamplePair {
+struct OrderedSamplePair<T> {
     
-    let index1: Int
-    let index2: Int
+    let index1: T
+    let index2: T
     let distance: Double
     
-    init(idx1: Int, idx2: Int, distance: Double = 1) {
+    init(idx1: T, idx2: T, distance: Double = 1) {
         self.distance = distance
         index1 = idx1
         index2 = idx2
     }
 }
 
-public struct Pair {
+struct Pair<T> {
     let index1: Int
     let index2: Int
     
@@ -48,73 +48,17 @@ public struct Pair {
     }
 }
 
-public class ChineseWhispers {
+
+public class ChineseWhisper {
     
-    public init() {
-        
-    }
+    public init() {}
     
-    func convertUnorderedToOrdered(edges: [SamplePair]) -> [OrderedSamplePair] {
-        var orderdPairs: [OrderedSamplePair] = []
-        orderdPairs.reserveCapacity(edges.count*2)
-        
-        for (i, _) in edges.enumerated() {
-            orderdPairs.append(OrderedSamplePair(idx1: edges[i].index1, idx2: edges[i].index2, distance: edges[i].distance))
-            if edges[i].index1 != edges[i].index2 {
-                orderdPairs.append(OrderedSamplePair(idx1: edges[i].index2, idx2: edges[i].index1, distance: edges[i].distance))
-            }
-        }
-        return orderdPairs.sorted { (a, b) -> Bool in
-            a.index1 < b.index1 || (a.index1 == b.index1 && a.index2 < b.index2)
-        }
-    }
-    
-    func findNeighborRanges(edges: [OrderedSamplePair]) -> [Pair] {
-        let numNodes: Int = maxIndexPlusOne(pairs: edges)
-        var neighbors: [Pair] = Array(repeating: Pair(idx1: 0, idx2: 0), count: Int(numNodes))
-        var cur_node: Int = 0
-        var start_idx: Int = 0
-        
-        for (index, value) in edges.enumerated() {
-            if value.index1 != cur_node {
-                neighbors[Int(cur_node)] = Pair(idx1: start_idx,
-                                                idx2: index)
-                start_idx = index
-                cur_node = value.index1
-            }
-        }
-        if !neighbors.isEmpty {
-            neighbors[Int(cur_node)] = Pair(idx1: start_idx,
-                                            idx2: edges.count)
-        }
-        return neighbors
-    }
-    
-    func maxIndexPlusOne(pairs: [OrderedSamplePair]) -> Int {
-        if pairs.count == 0 {
-            return 0
-        }else {
-            var max_idx: Int = 0
-            for (_, value) in pairs.enumerated() {
-                if value.index1 > max_idx {
-                    max_idx = value.index1
-                }
-                if value.index2 > max_idx {
-                    max_idx = value.index2
-                }
-            }
-            return max_idx + 1
-        }
-        
-    }
-    
-    
-    public func chinese_whispers<T>(
+    public func cluster<T>(
         objects: [T],
         distanceFunction: (T, T) -> Double,
         eps: Double,
         numIterations: Int) -> [Int] {
-        var edges: [SamplePair] = []
+        var edges: [SamplePair<Int>] = []
         guard !objects.isEmpty else {
             return []
         }
@@ -126,18 +70,18 @@ public class ChineseWhispers {
                 }
             }
         }
-        return chinese_whispers(edges: edges, numIterations: numIterations)
+        return cluster(edges: edges, numIterations: numIterations)
     }
     
-    public func chinese_whispers(
-        edges: [SamplePair],
+    func cluster(
+        edges: [SamplePair<Int>],
         numIterations: Int) -> [Int] {
-        let orderdEdges = convertUnorderedToOrdered(edges: edges)
-        return chinese_whispers(edges: orderdEdges, numIterations: numIterations)
+        let orderedEdges = convertUnorderedToOrdered(edges: edges)
+        return cluster(edges: orderedEdges, numIterations: numIterations)
     }
     
-    public func chinese_whispers(
-        edges: [OrderedSamplePair],
+    func cluster(
+        edges: [OrderedSamplePair<Int>],
         numIterations: Int) -> [Int] {
         let neighbors = findNeighborRanges(edges: edges)
         var labels: [Int] = Array<Int>(repeating: 0, count: neighbors.count)
@@ -184,7 +128,8 @@ public class ChineseWhispers {
         return labels
     }
     
-    public func group<T>(objects: [T], labels: [Int]) -> [[T]] {
+    func group<T>(objects: [T], labels: [Int]) -> [[T]] {
+        precondition(objects.count == labels.count)
         var cluster: [Int : [T]] = [:]
         for (i, value) in labels.enumerated() {
             if cluster[value] != nil {
@@ -195,6 +140,62 @@ public class ChineseWhispers {
         }
         return cluster.map { (_ , value) -> [T] in
            value
+        }
+    }
+}
+
+private extension ChineseWhisper {
+    
+    func convertUnorderedToOrdered(edges: [SamplePair<Int>]) -> [OrderedSamplePair<Int>] {
+        var orderedPairs: [OrderedSamplePair<Int>] = []
+        orderedPairs.reserveCapacity(edges.count*2)
+        
+        for (i, _) in edges.enumerated() {
+            orderedPairs.append(OrderedSamplePair(idx1: edges[i].index1, idx2: edges[i].index2, distance: edges[i].distance))
+            if edges[i].index1 != edges[i].index2 {
+                orderedPairs.append(OrderedSamplePair(idx1: edges[i].index2, idx2: edges[i].index1, distance: edges[i].distance))
+            }
+        }
+        return orderedPairs.sorted { (a, b) -> Bool in
+            a.index1 < b.index1 || (a.index1 == b.index1 && a.index2 < b.index2)
+        }
+    }
+    
+    func findNeighborRanges(edges: [OrderedSamplePair<Int>]) -> [Pair<Int>] {
+        let numNodes: Int = maxIndexPlusOne(pairs: edges)
+        var neighbours: [Pair<Int>] = Array(repeating: Pair(idx1: 0, idx2: 0), count: Int(numNodes))
+        var cur_node: Int = 0
+        var start_idx: Int = 0
+        
+        for (index, value) in edges.enumerated() {
+            if value.index1 != cur_node {
+                neighbours[Int(cur_node)] = Pair(idx1: start_idx,
+                                                idx2: index)
+                start_idx = index
+                cur_node = value.index1
+            }
+        }
+        if !neighbours.isEmpty {
+            neighbours[Int(cur_node)] = Pair(idx1: start_idx,
+                                            idx2: edges.count)
+        }
+        return neighbours
+    }
+    
+    func maxIndexPlusOne(pairs: [OrderedSamplePair<Int>]) -> Int {
+        if pairs.count == 0 {
+            return 0
+        }else {
+            var max_idx: Int = 0
+            for (_, value) in pairs.enumerated() {
+                if value.index1 > max_idx {
+                    max_idx = value.index1
+                }
+                if value.index2 > max_idx {
+                    max_idx = value.index2
+                }
+            }
+            return max_idx + 1
         }
     }
 }
--- Sources/LADSA/Algorithm/Clusters/DBSCAN.swift
+++ Sources/LADSA/Algorithm/Clusters/DBSCAN.swift
@@ -7,69 +7,82 @@
 
 import Foundation
 
-class DBSCAN<T:Hashable> {
+public class DBSCAN {
     
-    var db: [T]
-    var label: [T: String]
-    
-    init(aDB:[T])  {
-        
-        db = aDB
-        label = [T: String]()
-        for P in db {
-            self.label[P] = "undefined"
+    private class Point<Value: Equatable>: Equatable {
+        typealias Label = Int
+
+        let value: Value
+        var label: Label?
+
+        init(_ value: Value) {
+            self.value = value
+        }
+
+        static func == (lhs: Point, rhs: Point) -> Bool {
+            return lhs.value == rhs.value
         }
     }
-    
-    func DBSCAN(distFunc: (T, T)->Double, eps: Double, minPts: Int) {
-        var C = 0
-        for P in self.db {
-            
-            if self.label[P] != "undefined" {
-                continue
-            }
-            
-            var N = self.rangeQuery(distFunc: distFunc, Q: P, eps: eps)
-            
-            if N.count < minPts  {
-                
-                self.label[P] = "Noise"
-                continue
-            }
-            
-            C = C + 1
-            self.label[P] = "\(C)"
-            
-            for Q in N {
-                
-                if self.label[Q] == "Noise" {
-                    self.label[Q] = "\(C)"
-                }
-                
-                if self.label[Q] != "undefined" {
-                    continue
-                    
-                }
-                
-                label[Q] = "\(C)"
-                let N1 = self.rangeQuery(distFunc: distFunc, Q: Q, eps: eps)
-                if N1.count >= minPts  {
-                    
-                    N.append(contentsOf: N1)
-                    continue
+
+    public init() { }
+
+    /**
+     Clusters values according to the specified parameters.
+     - Parameters:
+    - values: values to be clustered
+       - epsilon: The maximum distance from a specified value
+                  for which other values are considered to be neighbours.
+       - minimumNumberOfPoints: The minimum number of points
+                                required to form a dense region.
+       - distanceFunction: A function that computes
+                           the distance between two values.
+     - Throws: Rethrows any errors produced by `distanceFunction`.
+     - Returns: A tuple containing an array of clustered values
+                and an array of outlier values.
+    */
+    public func cluster<Value: Equatable>(values: [Value], epsilon: Double,
+                               minimumNumberOfPoints: Int,
+                               distanceFunction: (Value, Value) throws -> Double) rethrows -> [[Value]] {
+        precondition(minimumNumberOfPoints >= 0)
+
+        let points = values.map { Point($0) }
+
+        var currentLabel = 0
+        for point in points {
+            guard point.label == nil else { continue }
+
+            var neighbors = try points.filter { try distanceFunction(point.value, $0.value) < epsilon }
+            if neighbors.count >= minimumNumberOfPoints {
+                defer { currentLabel += 1 }
+                point.label = currentLabel
+
+                while !neighbors.isEmpty {
+                    let neighbor = neighbors.removeFirst()
+                    guard neighbor.label == nil else { continue }
+
+                    neighbor.label = currentLabel
+
+                    let n1 = try points.filter { try distanceFunction(neighbor.value, $0.value) < epsilon }
+                    if n1.count >= minimumNumberOfPoints {
+                        neighbors.append(contentsOf: n1)
+                    }
                 }
             }
         }
-    }
-    
-    func rangeQuery(distFunc: (T,T)->Double, Q: T, eps: Double)-> [T] {
-        
-        var Neighbors = [T]()
-        for P in self.db {
-            if distFunc(Q, P) <= eps {
-                Neighbors.append(P)
+
+        var clusters: [[Value]] = []
+        var outliers: [Value] = []
+
+        for points in Dictionary(grouping: points, by: { $0.label }).values {
+            let values = points.map { $0.value }
+            if values.count == 1 {
+                outliers.append(contentsOf: values)
+            } else {
+                clusters.append(values)
             }
         }
-        return Neighbors
+
+        return (clusters)
     }
 }
+
